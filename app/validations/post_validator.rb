5402e268187cd80025e972923915922c4c623690
Richell Bonfield <richell.bonfield12@gmail.com>
2019-11-30 23:44:04 +0000

    Automatically backed up by Learn

--- app/validations/post_validator.rb
+++ app/validations/post_validator.rb
@@ -1,7 +1,7 @@
 class PostValidator < ActiveModel::Validator 
   def validate(record)
-    unless record.title.match?(/"Won't Believe"/ "Secret" / "Top[number]"/"Guess")
-    record.errors[:title] << "This title is not allowed!"
-  end
+    unless record.title.match?(/"Won't Believe"/ "Secret" /   "Top[number]"/"Guess")
+      record.errors[:title] << "This title is not allowed!"
+    end
   end 
 end 
