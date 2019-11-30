ab4854d3bde85f75cfd4b875383ff0a15b5139dd
Richell Bonfield <richell.bonfield12@gmail.com>
2019-11-30 23:42:58 +0000

    Automatically backed up by Learn

--- app/validations/post_validator.rb
+++ app/validations/post_validator.rb
@@ -1,4 +1,6 @@
 class PostValidator < ActiveModel::Validator 
   def validate(record)
+    unless record.title.match?(/"Won't Believe"/ "Secret" / "Top[number]"/"Guess")
+    record.errors[:]
   end 
 end 
